c9706db2395a311bbaa5a577135f98bc9e03d4b8
orlando-messias <programadorlando@gmail.com>
2020-06-01 16:37:50 -0300

    MovieList component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movie={data} />
+      <MovieList movies={data} />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const movies = this.props.movies;
+    return (
+      <div>
+        {movies.map(eachMovie => <MovieCard key={eachMovie.title} movie={eachMovie} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
