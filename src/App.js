23fdcd165a3832ebf188656bbffaf7a7ac102bea
deboracosilveira <deboracosilveira@gmail.com>
2020-06-01 17:38:17 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+// import MovieCard from './components/MovieCard';
+import MovieList from './components/MovieList';
+// import Rating from './components/Rating';
+import data from './data';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <MovieList movies={data} />
     </div>
   );
 }
