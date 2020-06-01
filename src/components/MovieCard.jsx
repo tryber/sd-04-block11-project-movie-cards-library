ab9e822c22d776dba56486329a2482a8ff197db8
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-06-01 18:28:20 -0300

    Adicionando Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import Rating from './Rating'
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
+    const { imagePath, title, subtitle, storyline, rating } = this.props.movie;
     return (
-      <div>
-        <img src={movie.imagePath} alt="Capa do filme" />
-        <h4>{movie.title}</h4>
-        <h5>{movie.subtitle}</h5>
-        <p>{movie.storyline}</p>
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt="Capa do filme" />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <div className="movie-card-rating">
+          <Rating rating={rating} />
+        </div>
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -5,7 +5,7 @@ class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
+      <div className="movie-list">
         {movies.map((element) => <MovieCard key={element.title} movie={element} />)}
       </div>
     );
