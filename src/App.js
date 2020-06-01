4050e44103f90856888716b6c2f6b82d205f6aab
Filipe de Castro Oliveira <filipeyoga@gmail.com>
2020-06-01 16:56:21 -0300

    Commit Bomba

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import movies from './data';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Valeu Procópio e Diegão!
+      <Header />
+      <MovieList movies={movies} />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+const MovieCart = ({ movie }) => {
+  const { title, subtitle, storyline, rating, imagePath } = movie;
+  return (
+    <div className="movie-card">
+      <img className="movie-card-image" src={imagePath} alt={title} />
+      <h4 className="movie-card-body">{title}</h4>
+      <h5 className="movie-card-subtitle">{subtitle}</h5>
+      <p className="movie-card-storyline">{storyline}</p>
+      <Rating rating={rating} />
+    </div>
+  );
+};
+
+export default MovieCart;
