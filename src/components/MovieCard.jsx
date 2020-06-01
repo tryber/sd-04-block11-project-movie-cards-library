51727f5f9b7d77d7ca7467f0f00e7d5b5477c4c6
juniomelos <you@example.com>
2020-06-01 16:53:07 -0300

    Test passed: Header, MovieList and part MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,24 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    // eslint-disable-next-line react/destructuring-assignment
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} alt={imagePath} />
+
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+
+        <p className="rating">{rating}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
