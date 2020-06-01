abd59597984ca041a5fefaaf6997da35e77caca5
orlando-messias <programadorlando@gmail.com>
2020-06-01 16:52:27 -0300

    Rating component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class MovieCard extends React.Component {
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline}</p>
         <img src={movie.imagePath} alt="movie" />
+        <Rating rating={movie.rating} />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,14 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <div>
+        Rating <div className="rating">{this.props.rating}</div>
+      </div>
+    );
+  }
+}
+
+export default Rating;
