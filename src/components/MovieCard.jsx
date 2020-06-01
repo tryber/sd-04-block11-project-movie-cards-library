172e270fe4e993e31d6e93c89fe0e14ab5322c25
frederico-campello <fredfmc33@gmail.com>
2020-06-01 17:20:43 -0300

    Iniciando o projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+
+
+    return (
+      <div>
+        <img src={imagePath} alt=''></img>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <p>Rating: {rating}</p>
+      </div>
+
+    )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+    
+    <div>
+        {movies.map(elemento => <MovieCard movie={elemento} key={elemento.title} />)}
+    </div>
+
+    )
+  }
+}
+
+export default MovieList;
