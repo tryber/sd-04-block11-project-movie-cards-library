e69a864367d2b1dbe6ad1c383339f2c8ff94b1cc
cacobribeiro <cacobribeiro@gmail.com>
2020-06-01 18:41:15 -0300

    Restante dos Requisitos corrigidos e funcionais, teste de CC a seguir

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,15 +5,15 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
     return (
-      <div className='movie-card-body'>
-        <div className='movie-card'>
-          <img alt={title} className='movie-card-image'>{imagePath}</img>
+      <div className='movie-card'>
+        <div className='movie-card-body'>
+          <img alt={title} className='movie-card-image' src={imagePath}/>
           <h4 className='movie-card-title'>{title}</h4>
           <h5 className='movie-card-subtitle'>{subtitle}</h5>
           <p className='movie-card-storyline'>{storyline}</p>
         </div>
         <div className='movie-card-rating'>
-          <Rating className='rating' rating={rating} />
+          <Rating rating={rating} />
         </div>
       </div>
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 class Rating extends React.Component {
   render() {
     return (
-      <div>
+      <div className='rating'>
         {this.props.rating}
       </div>
-    )
-  }
-}
+    );
+  };
+};
 export default Rating;
