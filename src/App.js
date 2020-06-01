1a7dd2aa816b67d60c1dca55483913ef9f409066
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-01 18:32:58 -0300

    Header - reqs 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
       Iniciando o projeto. VAMOS COM TUDO!
+      <Header />
     </div>
   );
 }
