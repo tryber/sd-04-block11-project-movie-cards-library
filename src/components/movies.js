b94e57ce2de351d120c6c8e947155ea220c0f628
joaosoutto <joaosoutto@hotmail.com>
2020-06-01 18:03:13 -0300

    oooo cc meu amigo, copera com nos

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,32 +1,31 @@
 const movies = [
-    {
-      title: 'Movie Title 1',
-      subtitle: 'The Lion King',
-      storyline:
-        'Este desenho animado da Disney mostra as aventuras de um leão jovem de nome Simba, o herdeiro de seu pai, Mufasa. O tio malvado de Simba, Oscar, planeja roubar o trono de Mufasa atraindo pai e filho para uma emboscada. Simba consegue escapar e somente Mufasa morre. Com a ajuda de seus amigos,Timon e Pumba, ele reaparece como adulto para recuperar sua terra, que foi roubada por seu tio Oscar.',
-      rating: 3,
-      imagePath:
-        'https://cdn.flickeringmyth.com/wp-content/uploads/2019/02/Lion-King-banner-1-600x350.jpg',
-    },
-    {
-      title: 'Movie Title 2',
-      subtitle: 'Bacurau',
-      storyline:
-        'Os moradores de um pequeno povoado do sertão brasileiro, chamado Bacurau, descobrem que a comunidade não consta mais em qualquer mapa. Aos poucos, percebem algo estranho na região: enquanto drones passeiam pelos céus, estrangeiros chegam à cidade. Quando carros se tornam vítimas de tiros e cadáveres começam a aparecer, Teresa, Domingas, Acácio, Plínio, Lunga e outros habitantes chegam à conclusão de que estão sendo atacados. Falta identificar o inimigo e criar coletivamente um meio de defesa.',
-      rating: 5.0,
-      imagePath:
-        'https://www.revide.com.br/media/upload/noticias/2020/01/20/08/52/poster-horizontal-800x445.jpeg',
-    },
-    {
-      title: 'Movie Title 3',
-      subtitle: 'The Silence of the Lambs',
-      storyline:
-        'Clarice Starling, uma das melhores estudantes da academia de treinamento do FBI. Jack Crawford quer que Clarice entreviste o Dr. Hannibal Lecter, um psiquiatra brilhante e também um psicopata violento, que cumpre prisão perpétua por vários atos de assassinato e canibalismo. Crawford acredita que Lecter pode ter uma visão em um caso e que Starling, como uma mulher jovem e atraente, pode ser a isca para atraí-lo.',
-      rating: 4.9,
-      imagePath:
-        'https://cdn.ticketsource.co.uk/images/promoter/banner/25704-1536343768045.jpg',
-    },
-  ];
-  
-  export default movies;
-  
+  {
+    title: 'Movie Title 1',
+    subtitle: 'The Lion King',
+    storyline:
+      'Este desenho animado da Disney mostra as aventuras de um leão jovem de nome Simba, o herdeiro de seu pai, Mufasa. O tio malvado de Simba, Oscar, planeja roubar o trono de Mufasa atraindo pai e filho para uma emboscada. Simba consegue escapar e somente Mufasa morre. Com a ajuda de seus amigos,Timon e Pumba, ele reaparece como adulto para recuperar sua terra, que foi roubada por seu tio Oscar.',
+    rating: 3,
+    imagePath:
+      'https://cdn.flickeringmyth.com/wp-content/uploads/2019/02/Lion-King-banner-1-600x350.jpg',
+  },
+  {
+    title: 'Movie Title 2',
+    subtitle: 'Bacurau',
+    storyline:
+      'Os moradores de um pequeno povoado do sertão brasileiro, chamado Bacurau, descobrem que a comunidade não consta mais em qualquer mapa. Aos poucos, percebem algo estranho na região: enquanto drones passeiam pelos céus, estrangeiros chegam à cidade. Quando carros se tornam vítimas de tiros e cadáveres começam a aparecer, Teresa, Domingas, Acácio, Plínio, Lunga e outros habitantes chegam à conclusão de que estão sendo atacados. Falta identificar o inimigo e criar coletivamente um meio de defesa.',
+    rating: 5.0,
+    imagePath:
+      'https://www.revide.com.br/media/upload/noticias/2020/01/20/08/52/poster-horizontal-800x445.jpeg',
+  },
+  {
+    title: 'Movie Title 3',
+    subtitle: 'The Silence of the Lambs',
+    storyline:
+      'Clarice Starling, uma das melhores estudantes da academia de treinamento do FBI. Jack Crawford quer que Clarice entreviste o Dr. Hannibal Lecter, um psiquiatra brilhante e também um psicopata violento, que cumpre prisão perpétua por vários atos de assassinato e canibalismo. Crawford acredita que Lecter pode ter uma visão em um caso e que Starling, como uma mulher jovem e atraente, pode ser a isca para atraí-lo.',
+    rating: 4.9,
+    imagePath:
+      'https://cdn.ticketsource.co.uk/images/promoter/banner/25704-1536343768045.jpg',
+  },
+];
+
+export default movies;
