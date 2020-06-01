f8006a67e8134fa5e0bb8e159d6163573ca7ff6e
deboracosilveira <deboracosilveira@gmail.com>
2020-06-01 18:18:29 -0300

    components 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
 import MovieList from './components/MovieList';
-// import Rating from './components/Rating';
 import data from './data';
 
 function App() {
