a80120a5c38a3e99e6c5f55082fa85d48f7538c2
Eduardo Santos <eduardo.debest81@gmail.com>
2020-06-01 16:54:20 -0300

    CC Fixes: the awakening

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MovieList from './components/MovieList';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       <MovieList movies={data} />
     </div>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,7 +7,7 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div className="movie-card" >
-        <img className="movie-card-image" alt="movie image" src={imagePath} />
+        <img className="movie-card-image" alt="movie" src={imagePath} />
         <div className="movie-card-body">
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
