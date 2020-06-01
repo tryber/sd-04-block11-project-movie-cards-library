ad49bd7b29a4ed851dfe24f43795fcec63c1c103
Leandro Marten <leandromarten1@gmail.com>
2020-06-01 17:24:56 -0300

    CC Fix Issues

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,11 +4,13 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    return ( 
+    return (
       <div className="movie-list">
-      { movies.map((movie) => <MovieCard key={movie.title} movie={movie}/>) }
+        {movies.map((movie) => (
+          <MovieCard key={movie.title} movie={movie} />
+        ))}
       </div>
-    )
+    );
   }
 }
 
