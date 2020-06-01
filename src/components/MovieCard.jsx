f26f0bc3f2f55993652ac45a504d3a4c06640008
djonata-dias <diasdjonata@gmail.com>
2020-06-01 19:16:48 -0300

    Criei o componente movieCard que cria a div com o card do filme com as informações dinamicamente

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>{this.props.title}</h2>
+          <h4>{this.props.subtitle}</h4>
+          <h5>{this.props.rating}</h5>
+          <p>{this.props.storyline}</p>
+        </div>
+        <div>
+          <img src={this.props.src} alt={this.props.alt} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
