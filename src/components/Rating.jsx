ee6a24912e2b1265950128b97ebaffc7902aa051
procopio420 <lucas-procopio@outlook.com>
2020-06-01 16:37:49 -0300

    bora cc! 2

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Rating = ({ rating }) => {
-  return (
-    <div className="rating">
-      <p>{rating}</p>
-    </div>
-  );
-};
+const Rating = ({ rating }) => (
+  <div className="rating">
+    <p>{rating}</p>
+  </div>
+);
 
 export default Rating;
