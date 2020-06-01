defae80d81ebe90781d6e718478f90d3a116f792
samueap <samueap@gmail.com>
2020-06-01 18:33:35 -0300

    indentacao

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,21 +6,21 @@ class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-    <div className="movie-card">
-      <div className="movie-card-body">
-        <img src={imagePath} className="movie-card-image" alt={title} />
-        <h4 className="movie-card-title">
-          {title}
-        </h4>
-        <h5 className="movie-card-subtitle">
-          {subtitle}
-        </h5>
-        <p className="movie-card-storyline">
-          {storyline}
-        </p>
-        <Rating className="rating" rating={rating} />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img src={imagePath} className="movie-card-image" alt={title} />
+          <h4 className="movie-card-title">
+            {title}
+          </h4>
+          <h5 className="movie-card-subtitle">
+            {subtitle}
+          </h5>
+          <p className="movie-card-storyline">
+            {storyline}
+          </p>
+          <Rating className="rating" rating={rating} />
+        </div>
       </div>
-    </div>
     );
   }
 }
