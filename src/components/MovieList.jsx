9a0569b99c42a805831aea3b49189ef50fc293ea
Alexandre Anicio <alexandre.anicio94@gmail.com>
2020-06-01 17:47:19 -0300

    CC not working

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((film) => (
+          <MovieCard key={film.title} movie={film} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
---
+++ src/styles/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const HeaderStyle = styled.div`
+  height: 10vh;
+  background: linear-gradient(#ffffff, #e5ecf4);
+`;
