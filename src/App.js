82a62bb744ef3b6bc525d9b009dc1e590453b8f4
djonata-dias <diasdjonata@gmail.com>
2020-06-01 19:19:07 -0300

    Componente App com os componentes auxiliares integrados para renderizar no index finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './App.css';
+import movies from './data';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import MovieCard from './components/MovieCard';
+import Rating from './components/Rating';
+
+const movie = movies.map(({ title, subtitle, storyline, imagePath, rating }) => (
+  <li>
+    <MovieCard
+      title={title}
+      subtitle={subtitle}
+      storyline={storyline}
+      src={imagePath}
+      alt={title}
+      rating={<Rating rating={rating} />}
+    />
+
+  </li>
+));
 
 function App() {
   return (
     <div className="App">
-      <Header text={'Movie Cards Library'} />
+      <Header text="Movie Cards Library" />
+      <MovieList movies={movie} />
     </div>
   );
 }
