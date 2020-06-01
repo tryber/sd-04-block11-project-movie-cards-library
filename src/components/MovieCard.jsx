77abe6c93ffd4325568df64eaa400b6efcf9865e
samueap <samueap@gmail.com>
2020-06-01 18:28:18 -0300

    add requistos e fix to cc

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
 // implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+    <div className="movie-card">
+      <div className="movie-card-body">
+        <img src={imagePath} className="movie-card-image" alt={title} />
+        <h4 className="movie-card-title">
+          {title}
+        </h4>
+        <h5 className="movie-card-subtitle">
+          {subtitle}
+        </h5>
+        <p className="movie-card-storyline">
+          {storyline}
+        </p>
+        <Rating className="rating" rating={rating} />
+      </div>
+    </div>
+    );
+  }
+}
+
+export default MovieCard;
