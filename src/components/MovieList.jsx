9f301c5384a808582874e3417f256d40bcb99fa7
cacobribeiro <cacobribeiro@gmail.com>
2020-06-01 17:20:25 -0300

    Requisito 3 , 80% ok.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
-// implement MovieList component here
+import React from 'react';
+import data from '../data';
+
+class MoviesList extends React.Component {
+  render() {
+    const { title, subtitle, storyline, ranting, imagePath } = this.props.movies;
+
+
+  }
+}
