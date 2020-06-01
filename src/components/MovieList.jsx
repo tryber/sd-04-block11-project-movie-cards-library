761c4530f1737c39bb318353b4ac3171bc1c6fe1
djonata-dias <diasdjonata@gmail.com>
2020-06-01 19:17:46 -0300

    Criado componente movieList que cria a div com o card que sera utilizado no map no App.js

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.movies}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
