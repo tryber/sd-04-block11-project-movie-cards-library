aa9de53b53b36777fa90b2b1db2d4ff03f954a6e
ederusena <eder.sena@live.com>
2020-06-01 19:21:30 -0300

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,9 @@
-// implement Header component here
+import React from "react";
+
+export default _ => {
+  return (
+      <header class='header'>
+             <h1>Movie Cards Library</h1>
+      </header>
+    )
+}
