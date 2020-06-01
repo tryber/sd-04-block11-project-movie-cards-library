143230fa81588fc07091783dc93f07eb7632fce2
Caio Magalhães <caio.lmf@gmail.com>
2020-06-01 17:46:47 -0300

    Implements MovieCard and Rating components

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,16 @@
-// implement Rating component here
+import React from 'react'
+
+class Rating extends React.Component {
+  render() {
+    const { ratingValue } = this.props;
+
+    return (
+      <div className="movie-card-rating">
+        Rating
+        <div className="rating">{ratingValue}</div>
+      </div>
+    );
+  }
+}
+
+export default Rating;
