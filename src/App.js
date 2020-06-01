530ea55942b716241588cf22679245120cfaefcf
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-01 17:43:41 -0300

    CC fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import movies from './data'
-// import './components/Header'; Why does't work
+import movies from './data';
+// import './components/Header'; Why does't work ?
 import './components/MovieList';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
@@ -10,9 +10,7 @@ import MovieList from './components/MovieList';
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
-      <div className='header'><Header></Header></div>
+      <Header className="header"></Header>
       <MovieList movies={movies} />
     </div>
   );
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,8 +3,13 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { imagePath, title } = this.props.movie;
     return (
-      <p>Temp</p>
+      <div className='moviecard'>
+        <p>Temp</p>
+        <img src={imagePath} alt={`${title} movie image`} />
+
+      </div>
     );
   }
 }
