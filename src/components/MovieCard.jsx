430ce2bfbba8029bc5288410adb28041ad984d40
orlando-messias <programadorlando@gmail.com>
2020-06-01 16:05:19 -0300

    MovieCard component

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const movie = this.props.movie;
+    return (
+      <div>
+        <h4>{movie.title}</h4>
+        <h5>{movie.subtitle}</h5>
+        <p>{movie.storyline}</p>
+        <img src={movie.imagePath} alt="movie" />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
