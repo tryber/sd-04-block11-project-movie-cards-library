b6f15bcee7a13ecec1bcf4936278b1260ed9b7ce
djonata-dias <diasdjonata@gmail.com>
2020-06-01 19:16:03 -0300

    Criado o componente Heade que monta o título do site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <header><h1>{this.props.text}</h1></header>;
+    return (
+      <header>
+        <h1>
+          {this.props.text}
+        </h1>
+      </header>
+    );
   }
 }
 
