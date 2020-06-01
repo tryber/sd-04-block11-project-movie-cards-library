18d3a5353b017905f7c03ff93fd4f778461f485a
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-01 16:22:33 -0300

    header ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return <header className='cabecalho'><h1>Movie Card Library</h1></header>;
-    }
+  render() {
+    return <header className='cabecalho'><h1>Movie Card Library</h1></header>
+  }
 }
 
 export default Header;
