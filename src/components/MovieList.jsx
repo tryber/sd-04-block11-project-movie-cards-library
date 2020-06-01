789ce816b336881b926f3a828d8032da668ec047
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-01 17:39:56 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    console.log(this.props);
+    const { movies } = this.props;
+    console.log(movies);
+    return (
+      <div/>
+    );
+  }
+}
+
+export default MovieList;
