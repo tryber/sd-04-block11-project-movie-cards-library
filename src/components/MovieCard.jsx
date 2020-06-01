40226e5e9e22cae28f90790d069b164a9ff919ff
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-01 18:06:47 -0300

    criado MovieCard e alterado MovieList

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,13 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+	render() {
+		const { title, subtitle, storyline, rating, imagePath } = this.props.movieCard;
+		return (
+			<section>aaa</section>
+		);
+	}
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,14 @@
 // implement MovieList component here
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    console.log(this.props);
     const { movies } = this.props;
-    console.log(movies);
     return (
-      <div/>
+      <section>
+        {movies.map((movie) => <MovieCard key={movie.title} movieCard={movie} />)}
+      </section>
     );
   }
 }
