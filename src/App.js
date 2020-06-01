4a2028f749ef151099b3e4184fc9cda0eab4037b
Julio Cesar <stininmatic@hotmail.com>
2020-06-01 20:58:24 -0300

    Finalizando

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header.jsx'
-import MovieList from './components/MovieList';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header.jsx";
+import MovieList from "./components/MovieList";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <MovieList />
     </div>
   );
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,19 @@
 // implement MovieCard component here
-import React from 'react'
-import Rating from './Rating.jsx'
+import React from "react";
+import Rating from "./Rating.jsx";
 
 class MovieCard extends React.Component {
-    render() {
-      return (
-          <div>
-              <img src={this.props.movie.imagePath} alt={this.props.movie.subtitle}/>
-              <h4>{this.props.movie.title}</h4>
-              <h5>{this.props.movie.subtitle}</h5>
-              <p>{this.props.movie.storyline}</p>
-              <Rating rating={this.props.movie.rating}/>
-          </div>
-      )
-    }
+  render() {
+    return (
+      <div>
+        <img src={this.props.movie.imagePath} alt={this.props.movie.subtitle} />
+        <h4>{this.props.movie.title}</h4>
+        <h5>{this.props.movie.subtitle}</h5>
+        <p>{this.props.movie.storyline}</p>
+        <Rating rating={this.props.movie.rating} />
+      </div>
+    );
   }
-  
-  export default MovieCard;
+}
+
+export default MovieCard;
