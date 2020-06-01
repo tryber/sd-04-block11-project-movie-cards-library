aaa851b2ddf7062b2da429d6cc7f77371f87a231
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-06-01 17:18:27 -0300

    CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -6,7 +6,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <div>
-        {movies.map((element) => <MovieCard />)}
+        {movies.map((element) => <MovieCard key={element.title} />)}
       </div>
     );
   }
