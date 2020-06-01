00ad924597950fe8bfda9ffec849e2fb4d699f1e
djonata-dias <diasdjonata@gmail.com>
2020-06-01 19:36:50 -0300

    CC

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import MovieList from './components/MovieList';
 import MovieCard from './components/MovieCard';
 import Rating from './components/Rating';
 
-const MovieCard = movies.map(({ title, subtitle, storyline, imagePath, rating }) => (
+const MovieCards = movies.map(({ title, subtitle, storyline, imagePath, rating }) => (
   <li>
     <MovieCard
       key={title}
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={MovieCard} />
+      <MovieList movies={MovieCards} />
     </div>
   );
 }
