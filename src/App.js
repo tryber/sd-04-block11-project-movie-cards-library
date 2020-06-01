49e64a13ead2558faf000557effd7f133fb68675
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-01 17:25:45 -0300

    up to request 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import movies from './data'
+// import './components/Header'; Why does't work
+import './components/MovieList';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+
 
 function App() {
   return (
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <div className='header'><Header></Header></div>
+      <MovieList movies={movies} />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+    render() {
+        return (
+            <div className="movielist">
+                {this.props.movies.map(movie => <MovieCard key={movie.title} movie={movie} />)}
+            </div>
+        );
+    }
+}
+export default MovieList;
