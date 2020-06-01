839c7622faa653700dc64cad5ffac273cd80ebb8
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-06-01 16:26:30 -0300

    Requisitos 1, 2 e 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,9 @@
-// implement MovieList component here
+import React from 'react'
+
+class MovieList extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movies;
+  }
+}
+
+export default MovieList;
