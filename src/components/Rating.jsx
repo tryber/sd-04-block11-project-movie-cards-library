d939af8655e035a91569e42199446ee14c847e7c
JorgeHSOsiro <jojimurakabe@gmail.com>
2020-06-01 18:17:08 -0300

    fixing issues

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,9 @@
-import React from 'react';;
+import React from 'react';
 
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
-    return <div className="rating">{rating}</div>
+    return <div className="rating">{rating}</div>;
   }
 }
 
