8b58087be273077bd02b68ce5e92b0c470a7a13b
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-01 18:24:48 -0300

    Estrutura concluída. Falta o CSS.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((mv) => <MovieCard key={mv.title} movie={mv} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
