011c6bca757e9723e9637cb1bba099b8a4cca6e2
Pedro Pires <pedrotpoliveira@gmail.com>
2020-06-02 11:48:32 -0300

    Final Commit - 1st Attempt

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,20 @@
 // implement MovieList component here
 import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
 
 function MovieList() {
   return (
     <main className="movie-list">
-      {/* <MovieCard /> */}
+      {movies.map((mov) => (
+        <MovieCard
+          title={mov.title}
+          subtitle={mov.subtitle}
+          rating={mov.rating}
+          storyline={mov.storyline}
+          imagePath={mov.imagePath}
+        />
+      ))}
     </main>
   );
 }
