1dba0771799ef40b1aba7f60b858db18de4ebc17
wistyk <leonhard@pop-os.localdomain>
2020-06-02 10:33:23 -0300

    CC

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-          <header>
-              <h1 className='shallow'>Movie Cards Library</h1>
-            </header>
-        );
-    }
+      <header>
+        <h1 className="shallow">Movie Cards Library</h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
