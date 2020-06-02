251bdd6def765833e5fa61ac0242f74700bc9b98
AliceAAtalla <aliceatalla36@gmail.com>
2020-06-02 08:05:56 -0300

    Codeclimate 4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
+import moviesArray from './data';
 
 function App() {
   return (
