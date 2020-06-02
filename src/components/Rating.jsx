7c56387cf39ba64a9fe6665d7bbc931bb7611d79
danilossousa <danilosantossousa@gmail.com>
2020-06-02 13:11:14 -0300

    v0.16.0.2

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,13 @@
 // implement Rating component here
-import React, { Component }  from 'react';
+import React, { Component } from 'react';
 
 class Rating extends Component {
   render() {
     const rating = this.props.rating;
     return (
-    <div className="rating">
-      <p>{rating}</p>
-    </div>
+      <div className="rating">
+        <p>{rating}</p>
+      </div>
     );
   }
 }
