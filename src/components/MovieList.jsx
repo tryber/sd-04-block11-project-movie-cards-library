46057b776c17db5413b59e016a830501ed648f68
AliceAAtalla <aliceatalla36@gmail.com>
2020-06-02 08:03:18 -0300

    Codeclimate 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-	render() {
-		return (
-			<div className="movie-list">
-				{this.props.movies.map((movieElem) => (
-					<MovieCard key={movieElem.title} movie={movieElem} />
-				))}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="movie-list">
+        {this.props.movies.map((movieElem) => (
+          <MovieCard key={movieElem.title} movie={movieElem} />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class Rating extends React.Component {
-	render() {
-		return (
-			<div className="movie-card-rating">
-				<p className="rating">{this.props.rating}</p>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="movie-card-rating">
+        <p className="rating">{this.props.rating}</p>
+      </div>
+    );
+  }
 }
 
 export default Rating;
