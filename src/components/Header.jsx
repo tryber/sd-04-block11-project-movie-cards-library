89a8d6aa396d295fdf61cc5a63e02c77b5bb6318
danilossousa <danilosantossousa@gmail.com>
2020-06-02 11:12:06 -0300

    v0.2.0.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <p>hi</p>
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
