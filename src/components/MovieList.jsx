ba35a0b0a65f263e07ed137f035aa53c2fbd0a15
igorgbr <igorgphotoarte@gmail.com>
2020-06-01 21:21:17 -0300

    agora contra o cc

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+    render() {
+        const { movies } = this.props;
+
+        return (
+            
+            movies.map((mv) => <div className="movie-card"><MovieCard key={mv.title} movie={mv} /> </div>)
+        )
+        
+    }
+
+}
+
+export default MovieList;
