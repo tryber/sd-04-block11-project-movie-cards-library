0188731ccdcde5ebbf9ef5a3a4cf8868570aa3c8
igorgbr <igorgphotoarte@gmail.com>
2020-06-01 21:35:20 -0300

    agora contra o cc 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,16 +3,14 @@ import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
-    render() {
-        const { movies } = this.props;
+      render() {
+          const { movies } = this.props;
 
-        return (
-            
-            movies.map((mv) => <div className="movie-card"><MovieCard key={mv.title} movie={mv} /> </div>)
-        )
-        
+          return (
+            movies.map((mv) => <div className="movie-card ">
+                <MovieCard key={mv.title} movie={mv} /> </div>)
+        );
     }
-
 }
 
 export default MovieList;
