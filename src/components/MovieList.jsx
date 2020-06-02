33bdeef88bf752bfd100cb9b42cb8f6cc37cd6b0
Pedro Pires <pedrotpoliveira@gmail.com>
2020-06-02 10:58:49 -0300

    MovieList Implementation

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+
+function MovieList() {
+  return (
+    <main className="movie-list">
+      {/* <MovieCard /> */}
+    </main>
+  );
+}
+
+export default MovieList;
