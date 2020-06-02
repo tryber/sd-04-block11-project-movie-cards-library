451a8ba9356509395982655e01988921de670c48
igorgbr <igorgphotoarte@gmail.com>
2020-06-01 21:59:42 -0300

    agora contra o cc 4

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 class Rating extends React.Component {
   render() {
     return (
-        <div className="movie-card-rating">
-            <p className="rating">{this.props.rating}</p>
-        </div>
-      );
+      <div className="movie-card-rating">
+          <p className="rating">{this.props.rating}</p>
+      </div>
+    );
   }
 }
 
