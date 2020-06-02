3ae9e21862ca6e1608cc8172c36c648dc2e629db
danilossousa <danilosantossousa@gmail.com>
2020-06-02 12:19:41 -0300

    v0.7.0.1

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    const movie = this.props.movie;
+    return (
+      <div>
+        <img src={movie.imagePath} alt="Filme"/>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import MovieCard from './MovieCard.jsx';
 
 class MovieList extends Component {
   render() {
     const movies = this.props.movies;
     return (
       <div>
-        {movies}
+        {movies.map(mov => <MovieCard key={mov.title} movie={mov} />)}
       </div>
     );
   }
