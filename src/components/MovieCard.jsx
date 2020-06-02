501b9836be0ce2d277675b45330aafc6a660cef0
Julio Cesar <stininmatic@hotmail.com>
2020-06-01 22:13:37 -0300

    Testando CC

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,16 @@
 // implement MovieCard component here
-import React from 'react'
-import Rating from './Rating.jsx'
+import React from 'react';
+import Rating from './Rating.jsx';
 
 class MovieCard extends React.Component {
-    render() {
+  render() {
       return (
-          <div>
-              <img src={this.props.movie.imagePath} alt={this.props.movie.subtitle}/>
-              <h4>{this.props.movie.title}</h4>
-              <h5>{this.props.movie.subtitle}</h5>
-              <p>{this.props.movie.storyline}</p>
-              <Rating rating={this.props.movie.rating}/>
+        <div>
+            <img src={ this.props.movie.imagePath } alt={ this.props.movie.subtitle } />
+              <h4>{ this.props.movie.title }</h4>
+              <h5>{ this.props.movie.subtitle }</h5>
+              <p>{ this.props.movie.storyline }</p>
+              <Rating rating={ this.props.movie.rating }/>
           </div>
       )
     }
