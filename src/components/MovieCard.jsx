0a4e3a0a68b25fbf844097d03a01742a2dd2695c
Julianafatsil <julianafatsil@gmail.com>
2020-06-02 00:12:32 -0300

    Create item 1 for item 7, start create components Header, MovieList and MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div>
+          <img src={imagePath} alt={title}/>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard'
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props; // destructuring
+    return (
+      <div>
+        {movies.map((movieMap) => // s/bigodes
+          <MovieCard key={movieMap.title}
+            movie={movieMap} />
+        )}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
+
