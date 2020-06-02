bd91ec2bbb90746373e9a8be30f53c09fdeffda9
wistyk <leonhard@pop-os.localdomain>
2020-06-02 19:33:29 -0300

    Segundo Describe

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+
+    return (
+      <p>
+        {title},
+        {subtitle},
+        {storyline},
+        {rating},
+        {imagePath},
+      </p>
+    );
+  }
+}
+
+export default MovieCard; 
