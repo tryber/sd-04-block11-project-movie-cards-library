ed13871eef6ffdecabe06755434c9ec1b325b852
danilossousa <danilosantossousa@gmail.com>
2020-06-02 09:55:36 -0300

    v0.1.0.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,12 @@
 // implement Header component here
+import React, {Component} from 'react';
 
+class Header extends Component {
+  render() {
+    return (
+      <p>hi</p>
+    );
+  }
+}
+
+export default Header;
