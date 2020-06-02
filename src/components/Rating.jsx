bd9d8fd4619e1b407fe6fad338191b87c79c6dba
djonata-dias <diasdjonata@gmail.com>
2020-06-01 21:05:12 -0300

    100% finalmente

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,7 +4,7 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <p className="rating">Popularidade do filme: {rating}</p>
+      <p className="rating">{rating}</p>
     );
   }
 }
