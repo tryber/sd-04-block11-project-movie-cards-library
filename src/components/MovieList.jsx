55f5b1b574034910f0fabbb8055778e69c45e220
Victor L <victor.lustosacp@gmail.com>
2020-06-02 14:41:51 -0300

    CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const{movies} = this.props;
+    const { movies } = this.props;
     return (
       <div className="movie-list">
         {movies.map((element) => <MovieCard key={element.title} movie={element} />)}
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-		const {rating} = this.props
+    const { rating } = this.props;
     return (
-      <div className="movie-card-rating rating">
-				{rating}
+      <div className="rating movie-card-rating">
+        { rating }
       </div>
     );
   }
