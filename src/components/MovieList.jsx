70dde7123f846f1fa293c7a7be3dbe8dc988ab1f
Victor L <victor.lustosacp@gmail.com>
2020-06-02 14:35:55 -0300

    CC

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
 		const {movies} = this.props;
 		return (
 			<div>
-				{movies.map(element => <MovieCard key={element.title} movie={element}/>)}
+				{movies.map(element => <MovieCard key={element.title} movie={element} />)}
 			</div>
 		)
 	}
