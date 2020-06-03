e7d13bf9d97e9e52e12a4d00c724dcac2d14ed36
André Ferreira <andrefpires1@gmail.com>
2020-06-03 11:06:57 -0300

    componente Header | Code Climate 3 (identation)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-      return (
-        <header className="movie-cards-library">
-            <h1 classsName="page-title">Movie Cards Library</h1>
-        </header>
-      );
+    return (
+      <header className="movie-cards-library">
+        <h1 classsName="page-title">Movie Cards Library</h1>
+      </header>
+    );
   }
 }
 
