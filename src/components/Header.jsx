950cb697a4aebed259d43ac58fc84b908d390cb8
fogo5000 <lucaslucas2008@gmail.com>
2020-06-04 18:25:34 -0300

    Fix CC 1.1v

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
 export default class Header extends Component {
-	render() {
-		return (
-			<div>
-				<header><h1>Movie Cards Library</h1></header>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <header><h1>Movie Cards Library</h1></header>
+      </div>
+    );
+  }
 }
