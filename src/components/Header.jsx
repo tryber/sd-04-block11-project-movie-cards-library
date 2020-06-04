cbc35f61d17be05a8499f1a7d7b06b667d006451
PedroParreiras <44480402+PedroParreiras@users.noreply.github.com>
2020-06-04 12:36:47 -0300

    end

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <header> Teste </header>;
+    return (
+      <header className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </header>
+    );
   }
 }
 
