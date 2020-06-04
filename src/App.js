0c79aeac5ecc04b69292c1087f84ba0f5ccf5074
fogo5000 <lucaslucas2008@gmail.com>
2020-06-04 18:16:47 -0300

    Fix CC 1.0v

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header'
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Header/>
     </div>
   );
 }
