56364c3cb6ebe9a613a86b6776bd8d58bfce4dcd
fogo5000 <lucaslucas2008@gmail.com>
2020-06-04 19:42:43 -0300

    Add requisition 3/4/10 1.0v

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+
+export default class MovieCard extends Component {
+  render() {
+    const {title, subtitle, storyline} = this.props.movie;
+    return (
+      <div>
+        <h2>{title}</h2>
+        <h4>{subtitle}</h4>
+        <p>{storyline}</p>
+      </div>
+    );
+  }
+}
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+export default class MovieList extends Component {
+  render() {
+    const {movies} = this.props;
+    return (
+      <div>
+        {movies.map((element, index) => <MovieCard key={index} movie={element}/>)}
+      </div>
+    );
+  }
+}
