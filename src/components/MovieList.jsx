4e29fa0d83c64cb717f9a90c17d207454459ddc1
ederusena <eder.sena@live.com>
2020-06-04 15:58:41 -0300

    MovieCard e MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+// Este componente representará toda a área com os cartões de filme
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section className="movie-list">
+        {this.props.movies.map((film) => <MovieCard key={film.title} movie={film} />)}
+      </section>
+    );
+  }
+}
+export default MovieList;
