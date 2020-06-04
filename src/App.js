99db5a9387ba748c9eb6b7eff12c559dc9fc2953
Tiago-ers <trsilva284@gmail.com>
2020-06-04 08:50:17 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Dados from './data'
+import Lista from './components/MovieList'
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />,
+      {<Lista dados={Dados}></Lista>}
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import Cards from './MovieCard';
+
+class MovieList extends React.Component {
+    render() {
+      const { movies } = this.props;
+        return (
+         <div>
+           {movies.map(item => <Cards movies={item} />)}
+         </div>
+        )
+    }
+}
+
+export default MovieList
