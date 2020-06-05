98000c3992a2e28d91f3810b78357678d64d2884
Josias Viveiro <josiasviveiro@gmail.com>
2020-06-05 17:33:35 -0300

    MovieCard - codeclimate

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Rating from './Rating';
 
-class MovieCard extends React.Component{
+class MovieCard extends React.Component {
 
   render() {
     const { movie } = this.props;
