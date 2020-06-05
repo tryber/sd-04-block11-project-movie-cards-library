c56676e42e87e1983c3787896ff76a8da807d00e
gabriellukke <gabriellukke@outlook.com>
2020-06-05 05:04:27 -0300

    Rating component created and rendered

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,12 +7,14 @@ class MovieCard extends React.Component {
     return (
       <div className="movie-card">
         <img src={movies.imagePath} alt={movies.title} className="movie-card-image" />
-        <div>
+        <div className="movie-card-body">
           <h4 className="movie-card-title">{movies.title}</h4>
           <h5 className="movie-card-subtitle">{movies.subtitle}</h5>
           <p className="movie-card-storyline">{movies.storyline}</p>
         </div>
-        <Rating rating={movies.rating} />
+        <div className="movie-card-rating">
+          <Rating rating={movies.rating} />
+        </div>
       </div>
     );
   }
