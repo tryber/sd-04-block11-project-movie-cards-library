6a9c1a4832893ed2ef2207b668df072d61171eb7
gabriellukke <gabriellukke@outlook.com>
2020-06-05 03:47:06 -0300

    Render the title of Header Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 class Header extends React.Component {
-  render() {}
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    )
+  }
 }
 
 export default Header;
