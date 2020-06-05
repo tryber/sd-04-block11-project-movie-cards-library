4d6f3f9177b90a8ec89e10cc600fc44a9858640f
saturnian99 <saturnian.dsg@gmail.com>
2020-06-05 19:37:35 -0300

    3) Created 'MovieList' component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
 
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">{this.props.movies}</div>
+    );
+  }
+}
+
+export default MovieList;
