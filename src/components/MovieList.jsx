56177624432e274ab25fd24545a4867245fbf6b8
Thabata <dornelasthabata@gmail.com>
2020-06-07 15:29:51 -0300

    Adicionando espaços entre as chaves da const movies

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,7 @@ import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const {movies} = this.props;
+    const { movies } = this.props;
     return (
       <div>
         {movies.map((movieElem) => (<MovieCard key={movieElem.title} movie={movieElem} />))}
