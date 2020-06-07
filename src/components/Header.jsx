4c18427f99f47f284a0722c09cf65372dec49331
Thabata <dornelasthabata@gmail.com>
2020-06-07 15:13:52 -0300

    Criando Header com h1 strong

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,16 @@
-// implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>
+          <strong>Movie Cards Library</strong>
+        </h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
 // testando
