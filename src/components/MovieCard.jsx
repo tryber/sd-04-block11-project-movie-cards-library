17c0a0550c3e79e88a71c2378a2ed98bb1ac63c2
rafaopbar <rafael.coper@outlook.com>
2020-06-08 11:19:16 -0300

    All tests running good

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,25 @@
-// implement MovieCard component here
+import React from 'react';
+import Rating from './Rating';
+import '../App.css';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={imagePath} alt={title} /><br />
+          <div><h4 className="movie-card-title">{title}</h4>
+            <h5 className="movie-card-subtitle">{subtitle}</h5>
+            <p className="movie-card-storyline">{storyline}</p>
+          </div>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={rating} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import '../App.css';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((e) => <MovieCard key={e.title} movie={e} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
