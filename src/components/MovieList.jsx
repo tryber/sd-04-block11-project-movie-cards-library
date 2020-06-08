0b47080acdd87253e9d190642cb9318e58dc8096
saturnian99 <saturnian.dsg@gmail.com>
2020-06-08 17:40:23 -0300

    4) Rendered 'MovieCard' components on 'MovieList' for each object at 'movies' array; 5) Added keys to 'MovieCard' components; 6) Created 'MovieCard' component

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import MovieCard from './MovieCard';
+
+/* function forEachMovie() {
+  return this.props.movies.map()
+} */
+
+function createMovieCard(movie) {
+  return (
+    <MovieCard key={movie.title} movie={movie} />
+  );
+}
 
 class MovieList extends React.Component {
   render() {
     return (
-      <div className="movie-list">{this.props.movies}</div>
+      <div className="movie-list">
+        {this.props.movies.map(movie => createMovieCard(movie))}
+      </div>
     );
   }
 }
