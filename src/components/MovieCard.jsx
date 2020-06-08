049c25ef8c4b6c83c8f89fca4876c5823e8f79aa
saturnian99 <saturnian.dsg@gmail.com>
2020-06-08 18:04:51 -0300

    7) Rendered movies images inside their respectives 'MovieCard' components

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+
     return (
-      <div className="movie-card" />
+      <div className="movie-card">
+        <img className="movie-card-image" src={imagePath} />
+      </div>
     );
   }
 }
